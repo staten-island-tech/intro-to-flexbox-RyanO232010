1ec748a4caff00dec6976ad5f047c35e3a6a726c
RyanO232010 <ryano92@nycstudents.net>
2025-10-08 12:30:21 -0400

    1

---
+++ index.js
@@ -0,0 +1,15 @@
+const card = {
+  id: "card",
+  title: "Kid Wig",
+  image: {
+    src: "pictures/kidwig.webp",
+    alt: "",
+    className: "wig",
+  },
+  button: {
+    className: "button",
+    text: "Buy",
+    textClassName: "description1",
+  },
+};
+
