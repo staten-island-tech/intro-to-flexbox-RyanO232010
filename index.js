28b0abd19d4ca29ee4c237bf1cfc75afa87e1ec9
RyanO232010 <ryano92@nycstudents.net>
2025-10-09 12:34:11 -0400

    1

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
-const card = {
-  id: "card",
-  title: "Kid Wig",
-  image: {
-    src: "pictures/kidwig.webp",
-    alt: "",
-    className: "wig",
+const Wig = [
+  {
+    name: "Adult Womens Wig",
+    price: 80.0,
+    category: "Adults",
+    img: "/pictures/women.png",
+    inStock: False,
   },
-  button: {
-    className: "button",
-    text: "Buy",
-    textClassName: "description1",
-  },
+];
+
+const DOMSelectors = {
+  container: document.querySelector("container"),
+  button: document.querySelector(".btn"),
+  price: document.querySelector(".price"),
+  picture: document.querySelector(".img")
+
 };
 
+
+
+
+
+
+
+
+function inject(card) {
+  const contianer = document.querySelector(".container")
+  DOMSelectors.display.insertAdjacentHTML(
+    "afterbegin",
+    `<div class="card">
+      <h1>"${card.name}"</h1>
+      <img class="img" src="${card.img}"/>
+      <button class="btn">Add to Cart</button>
+      <
+    </div>`
+  );
+}
+
+inject(Wig[0])
