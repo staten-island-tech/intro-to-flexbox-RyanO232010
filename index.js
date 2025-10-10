f3ec1d03f9429d7f172b38389e80c619ff93d292
RyanO232010 <ryano92@nycstudents.net>
2025-10-10 12:32:08 -0400

    1

--- index.js
+++ index.js
@@ -4,36 +4,61 @@ const Wig = [
     price: 80.0,
     category: "Adults",
     img: "/pictures/women.png",
-    inStock: False,
+    inStock: false,
+  },
+  {
+    name: "Adult Womens Wig",
+    price: 80.0,
+    category: "Adults",
+    img: "/pictures/women.png",
+    inStock: false,
+  },
+  {
+    name: "Adult Womens Wig",
+    price: 80.0,
+    category: "Adults",
+    img: "/pictures/women.png",
+    inStock: false,
+  },
+  {
+    name: "Adult Womens Wig",
+    price: 80.0,
+    category: "Adults",
+    img: "/pictures/women.png",
+    inStock: false,
+  },
+  {
+    name: "Adult Womens Wig",
+    price: 80.0,
+    category: "Adults",
+    img: "/pictures/women.png",
+    inStock: false,
   },
 ];
 
 const DOMSelectors = {
-  container: document.querySelector("container"),
+  name: document.querySelector(".name"),
+  container: document.querySelector(".container"),
+  card: document.querySelector(".card"),
   button: document.querySelector(".btn"),
   price: document.querySelector(".price"),
-  picture: document.querySelector(".img")
-
+  picture: document.querySelector(".img"),
 };
 
-
-
-
-
-
-
-
 function inject(card) {
-  const contianer = document.querySelector(".container")
-  DOMSelectors.display.insertAdjacentHTML(
+  const container = document.querySelector(".container");
+  DOMSelectors.container.insertAdjacentHTML(
     "afterbegin",
-    `<div class="card">
-      <h1>"${card.name}"</h1>
+    `
+      <div class="card">
+      <h1>${card.name}</h1>
       <img class="img" src="${card.img}"/>
+      <p class="price">$${card.price}</p>
       <button class="btn">Add to Cart</button>
-      <
-    </div>`
+      </div>
+`
   );
 }
 
-inject(Wig[0])
+
+Wig.forEach(inject);
