ceb8d2e43dfb7bfea84c4e364b854f4edf9c0780
RyanO232010 <ryano92@nycstudents.net>
2025-10-14 12:32:17 -0400

    1

--- index.js
+++ index.js
@@ -7,34 +7,84 @@ const Wig = [
     inStock: false,
   },
   {
-    name: "Adult Womens Wig",
+    name: "Adult Male Wig",
     price: 80.0,
     category: "Adults",
-    img: "/pictures/women.png",
+    img: "/pictures/men wig.jpg",
     inStock: false,
   },
   {
-    name: "Adult Womens Wig",
+    name: "Child Male Wig",
     price: 80.0,
     category: "Adults",
-    img: "/pictures/women.png",
+    img: "/pictures/kidwig.webp",
     inStock: false,
   },
   {
-    name: "Adult Womens Wig",
+    name: "Judge Male Wig",
     price: 80.0,
     category: "Adults",
-    img: "/pictures/women.png",
+    img: "/pictures/GWLA.webp",
     inStock: false,
   },
   {
-    name: "Adult Womens Wig",
+    name: "George Washington Ahh Wig",
     price: 80.0,
     category: "Adults",
-    img: "/pictures/women.png",
+    img: "/pictures/GWLAW.webp",
+    inStock: false,
+  },
+    {
+    name: "Witch Wig",
+    price: 80.0,
+    category: "Adults",
+    img: "/pictures/WITCH.webp",
+    inStock: false,
+  },
+    {
+    name: "Fancy Wig",
+    price: 80.0,
+    category: "Adults",
+    img: "/pictures/fancy.webp",
+    inStock: false,
+  },
+      {
+    name: "Marie Antoinette Wig",
+    price: 80.0,
+    category: "Adults",
+    img: "/pictures/MAW.webp",
+    inStock: false,
+  },
+        {
+    name: "Queen of Hearts Wig",
+    price: 80.0,
+    category: "Adults",
+    img: "/pictures/QOH.jpg",
+    inStock: false,
+  },
+          {
+    name: "Fancy Womens Wig",
+    price: 80.0,
+    category: "Adults",
+    img: "/pictures/fancyw.jpg",
+    inStock: false,
+  },
+        {
+    name: "Men's Wig",
+    price: 80.0,
+    category: "Adults",   
+    img: "/pictures/men.webp",
+    inStock: false,
+  },
+          {
+    name: "Liberal's Wig",
+    price: 80.0,
+    category: "Adults",   
+    img: "/pictures/liberal.webp",
     inStock: false,
   },
 ];
+
 
 const DOMSelectors = {
   name: document.querySelector(".name"),
@@ -43,6 +93,7 @@ const DOMSelectors = {
   button: document.querySelector(".btn"),
   price: document.querySelector(".price"),
   picture: document.querySelector(".img"),
+  inStock: document.querySelector(".inStock")
 };
 
 function inject(card) {
@@ -54,6 +105,7 @@ function inject(card) {
       <h1>${card.name}</h1>
       <img class="img" src="${card.img}"/>
       <p class="price">$${card.price}</p>
+      <p class="price">In Stock:${card.inStock}</p>
       <button class="btn">Add to Cart</button>
       </div>
 `
